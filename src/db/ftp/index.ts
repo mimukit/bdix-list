57eb51764de917a8afeb904090b42958d0586a34
Mukitul Islam Mukit <hellomukit@gmail.com>
2019-08-30 22:03:03 +0600

    feat: update ftp list

--- src/db/ftp/index.ts
+++ src/db/ftp/index.ts
@@ -4,20 +4,16 @@ export const ftpdb = [
     url: 'http://circleftp.net/',
   },
   {
-    name: 'Circle FTP 1 (Directory Listing)',
-    url: 'http://ftp1.circleftp.net/SERVER-1/DRIVE-1/',
-  },
-  {
     name: 'Circle FTP 2',
     url: 'http://ftp2.circleftp.net/',
   },
   {
-    name: 'Circle FTP 3',
-    url: 'http://ftp3.circleftp.net/',
-  },
-  {
     name: 'Circle HD',
     url: 'http://hd.circleftp.net/',
+  },
+  {
+    name: 'Alpha Media Zone FTP',
+    url: 'http://ftp.alphamediazone.com/',
   },
   {
     name: 'BOT-TOLA Cyber Net',
